ef692587e18a22cef420bf967c04588caa7610db
kiahDev <208305271+kiahDev@users.noreply.github.com>
2025-10-24 07:38:38 +0800

    did some styling

---
+++ script.js
@@ -0,0 +1,11 @@
+const addtask = document.querySelector('.enter');
+const task = document.querySelector('.task');
+const input = document.querySelector('.input');
+
+addtask.addEventListener('click', function() {
+    if (input.value === '') {
+        alert('Enter a task first');
+    } else {
+        
+    }
+});
