a379b3047eee834bcb6f9df239ada3f088e7dd5a
kiahDev <208305271+kiahDev@users.noreply.github.com>
2025-10-24 09:03:17 +0800

    added hover effect

--- script.js
+++ script.js
@@ -7,5 +7,25 @@ addtask.addEventListener('click', function() {
         alert('Enter a task first');
     } else {
         
+        const newD = document.createElement('div');
+        const newP = document.createElement('p');
+        const delbtn = document.createElement('button');
+
+        delbtn.textContent = 'Delete';
+
+        newP.textContent = input.value;
+        newD.append(newP, delbtn);
+
+        task.append(newD);
+        input.value = '';
+
+        delbtn.addEventListener('click', function() {
+            newD.remove();
+        })
+
+        newD.style.display = 'flex';
+        newD.style.justifyContent = 'space-between';
+        newD.style.alignItems = 'center';
+
     }
 });
